82a417231693a7d942ebe4f4f5912444d9546f14
Kevin Won <kevin.won91@gmail.com>
2024-02-06 00:59:37 -0600

    Lua: tables, ipairs. Love: keyboard

--- main.lua
+++ main.lua
@@ -1,3 +1,22 @@
+function love.load()
+  x = 100
+  fruits = {}
+  fruits = {"apple", "banana"}
+  table.insert(fruits, "pear")
+end
+
+function love.update(dt)
+  if love.keyboard.isDown("right") then
+    x = x + 400 * dt
+  end
+  if love.keyboard.isDown("left") then
+    x = x - 400 * dt
+  end
+end
+
 function love.draw()
-  love.graphics.print("Hello World", 400, 300)
+  love.graphics.rectangle("line", x, 40, 200, 150)
+  for i,fruit in ipairs(fruits) do
+    love.graphics.print(fruit, 100, 50 * i)
+  end
 end
